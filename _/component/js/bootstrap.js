f650be747923ba2e08b5b3781071843b34588eb5
ScarLight <cool64ash@gmail.com>
2014-05-20 08:38:00 +0800

    Update CSS
    1) Added fixed width for stock-item li a > span
    2) Added development folder
    3) custom js file removed trailing white space

---
+++ _/component/js/bootstrap.js
@@ -0,0 +1,22 @@
+//@prepros-append transition.js
+//@prepros-append alert.js
+//@prepros-append button.js
+//@prepros-append carousel.js
+//@prepros-append collapse.js
+//@prepros-append dropdown.js
+//@prepros-append modal.js
+
+//@prepros-append bootstrap-modalmanager.js
+//@prepros-append bootstrap-modal.js
+
+//@prepros-append tooltip.js
+//@prepros-append popover.js
+//@prepros-append scrollspy.js
+//@prepros-append tab.js
+//@prepros-append affix.js
+
+//@prepros-append bootstrap-select.js
+
+//@prepros-append fileinput.js
+
+//@prepros-append bootstrap-datepicker.js
